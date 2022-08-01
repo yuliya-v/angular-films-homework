a098e5e6cc3e0bb9ad8f36315d9b01d02d53ab8c
Yuliya Volchek <yuliavolchek@gmail.com>
2022-08-01 16:37:41 +0300

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CoreModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/core/components/header/language-select/language-select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-select',
+  templateUrl: './language-select.component.html',
+  styleUrls: ['./language-select.component.scss'],
+})
+export class LanguageSelectComponent {
+  constructor() {}
+}
