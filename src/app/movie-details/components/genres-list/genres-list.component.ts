70c2258fe7be2f9e438fd8e4c81985f7c5c11592
Yuliya Volchek <yuliavolchek@gmail.com>
2022-08-08 18:45:32 +0300

    update movie info & poster

--- src/app/movie-details/components/genres-list/genres-list.component.ts
+++ src/app/movie-details/components/genres-list/genres-list.component.ts
@@ -1,18 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { GenreService } from 'src/app/core/services/genre.service';
 
 @Component({
   selector: 'app-genres-list',
   templateUrl: './genres-list.component.html',
   styleUrls: ['./genres-list.component.scss'],
 })
-export class GenresListComponent implements OnInit {
-  @Input() genreIds: number[] = [];
-  genres: string[] = [];
+export class GenresListComponent {
+  @Input() genres: string[] = [];
 
-  constructor(private genreService: GenreService) {}
-
-  ngOnInit() {
-    this.genres = this.genreIds.map(id => this.genreService.getGenre(id));
-  }
+  constructor() {}
 }
--- src/app/movie-details/components/movie-info/movie-info.component.ts
+++ src/app/movie-details/components/movie-info/movie-info.component.ts
@@ -1,3 +1,4 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { MovieDetails } from 'src/app/core/models/movie-details.model';
 
@@ -5,6 +6,7 @@ import { MovieDetails } from 'src/app/core/models/movie-details.model';
   selector: 'app-movie-info',
   templateUrl: './movie-info.component.html',
   styleUrls: ['./movie-info.component.scss'],
+  providers: [CurrencyPipe],
 })
 export class MovieInfoComponent implements OnInit {
   @Input() data?: MovieDetails;
@@ -12,17 +14,38 @@ export class MovieInfoComponent implements OnInit {
   overview = '';
   releaseDate = '';
   duration = '';
-  genreIds: number[] = [];
+  budget = '';
+  revenue = '';
+  genres: string[] = [];
 
-  constructor() {}
+  constructor(private currencyPipe: CurrencyPipe) {}
 
   ngOnInit(): void {
     if (this.data) {
       this.title = this.data.title;
       this.overview = this.data.overview;
       this.releaseDate = this.data.release_date;
-      this.duration = `${this.data.runtime}`;
-      this.genreIds = this.data.genres.map(el => el.id);
+      this.duration = `${this.data.runtime / 60}`;
+      this.genres = this.data.genres.map(el => el.name);
+      [this.budget, this.revenue] = [this.data.budget, this.data.revenue].map(
+        this.transformCurrency.bind(this)
+      );
+      this.duration = this.transformDuration(this.data.runtime);
     }
+  }
+
+  private transformCurrency(amount: number): string {
+    return (
+      this.currencyPipe
+        .transform(amount, 'USD', undefined, '0.0-0')
+        ?.replace(/,/g, ' ')
+        .replace('$', '$ ') || ''
+    );
+  }
+
+  private transformDuration(duration: number): string {
+    const hours = Math.floor(duration / 60);
+    const minutes = duration - hours * 60;
+    return `${hours}:${minutes}`;
   }
 }
