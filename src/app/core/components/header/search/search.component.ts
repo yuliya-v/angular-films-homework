a4962fa0e346ebfced38aaf6685f52eb7495e168
Yuliya Volchek <yuliavolchek@gmail.com>
2022-08-25 20:25:19 +0300

    add search service

--- src/app/core/components/header/search/search.component.ts
+++ src/app/core/components/header/search/search.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter } from 'rxjs';
+import { SearchService } from 'src/app/core/services/search.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   public searchValue = new FormControl('');
   public form = new FormGroup({
     searchValue: this.searchValue,
   });
+
+  constructor(private searchService: SearchService) {}
+
+  ngOnInit() {
+    this.searchValue.valueChanges
+      .pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        filter(val => !!val && val.length > 1)
+      )
+      .subscribe(searchString => {
+        if (searchString) console.log(searchString);
+        // if (searchString) this.searchService.search(searchString);
+      });
+  }
 }
---
+++ src/app/core/services/search.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SearchService {
+  public search(query: string) {
+    return;
+  }
+}
